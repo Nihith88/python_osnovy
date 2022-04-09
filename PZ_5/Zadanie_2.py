eeb7337e40c393da97d620ae506e925db58761e4
Ilya Filisteev <97627249+Nihith88@users.noreply.github.com>
2022-04-09 08:11:36 +0300

    Devel (#3)
    * Выполнение ПЗ № 4
    
    * Выполнение практического задания № 5
    
    * Добавлен подсчет слов в каждой строке

--- PZ_5/Zadanie_2.py
+++ PZ_5/Zadanie_2.py
@@ -2,10 +2,12 @@
 2. Создать текстовый файл (не программно), сохранить в нём несколько строк, выполнить
 подсчёт строк и слов в каждой строке.
 """
-
+word_count = {}
 string_count = 0
-with open('user_2.txt', 'r') as usr_file:
-    for line in usr_file:
+with open('user_2.txt', mode='r', encoding='UTF-8') as usr_file:
+    for line in enumerate(usr_file):
+        word_count[line[0]] = len(line[1].split())
         string_count += 1
 print(f"В файле {usr_file.name} количество строк: {string_count}")
-
+for i in word_count.keys():
+    print(f"Строка {i}: слов - {word_count[i]}")
